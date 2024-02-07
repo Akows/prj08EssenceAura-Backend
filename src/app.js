5fbf8eed8707838602dd47c54b47511c857588fb
AKOWSPC <akows141@gmail.com>
2024-02-07 13:03:31 +0900

    Fix: S3로 배포한 프론트엔드에서 백엔드 호출이 불가능한 문제를 수정. (CORS 설정의 URL 주소를 배포 환경으로 변경하지 않았음.)

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ require('dotenv').config();
 
 // CORS 미들웨어 설정
 const corsOptions = {
-    origin: 'http://localhost:5173',
+    origin: 'http://essenceaura-production.s3-website.ap-northeast-2.amazonaws.com',
     credentials: true, // 이 옵션을 통해 쿠키를 함께 보낼 수 있도록 허가
     methods: 'GET, HEAD, PUT, PATCH, POST, DELETE',
     allowedHeaders: ['Content-Type', 'Authorization'],
