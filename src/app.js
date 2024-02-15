a012183dc95f841199d9af2577822d3a1f1bc227
AKOWSPC <akows141@gmail.com>
2024-02-15 18:23:24 +0900

    Fix: HTTPS 적용 이후 발생하는 문제들을 해결 중.. (CORS 관련 preflight 요청을 처리하기 위해 OPTIONS 메소드에 대한 처리를 추가.) (모든 응답에 CORS 관련 헤더를 명시적으로 포함시키기 위해 추가 미들웨어를 설정.)

--- src/app.js
+++ src/app.js
@@ -17,6 +17,18 @@ const corsOptions = {
     allowedHeaders: ['Content-Type', 'Authorization'],
 };
 
+app.options('*', cors(corsOptions)); // 모든 경로에 대한 OPTIONS 요청 허용
+
+app.use(cors(corsOptions));
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'https://essence-aura.com');
+    res.header('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    next();
+});
+
 // 로깅 미들웨어
 // app.use((req, res, next) => {
 //     console.log(`${new Date().toISOString()} - ${req.method} Request to ${req.originalUrl}`);
@@ -58,7 +70,6 @@ app.use((req, res, next) => {
     next();
 });
 
-app.use(cors(corsOptions));
 app.use(express.json());
 app.use(cookieParser());
 
